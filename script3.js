e5dd8c0546e8a98289dcf06abf35a41311bee186
KBHR-WEB <147394940+KBHR-WEB@users.noreply.github.com>
2023-10-11 12:53:25 +0000

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,7 @@
+document.getElementById('ouvrir').addEventListener('click', function() {
+    document.getElementById('fenetre').style.display = 'block';
+});
+
+document.getElementById('fermer').addEventListener('click', function() {
+    document.getElementById('fenetre').style.display = 'none';
+});
---
+++ script4.js
@@ -0,0 +1,7 @@
+document.getElementById('ouvrir2').addEventListener('click', function() {
+    document.getElementById('fenetre2').style.display = 'block';
+});
+
+document.getElementById('fermer2').addEventListener('click', function() {
+    document.getElementById('fenetre2').style.display = 'none';
+});
